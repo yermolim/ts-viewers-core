3f13b9b983bdd0486e0338876d035e712515d9de
Володимир Єрмоленко <yermolim@gmail.com>
2021-07-22 14:14:53 +0300

    edit styles template

--- src/assets/styles.ts
+++ src/assets/styles.ts
@@ -106,9 +106,7 @@ export const styles = /*html*/`
     align-items: center;
     flex-grow: 0;
     flex-shrink: 0;
-    left: calc(50% - 200px);
     bottom: 20px;
-    width: 400px;
     height: 50px;  
     background: var(--${appName}-color-primary-tr-final);
     box-shadow: 0 0 10px var(--${appName}-color-shadow-final);
@@ -119,10 +117,6 @@ export const styles = /*html*/`
     bottom: 0;
     height: 0;
     transition: bottom 0.1s linear 0.1s, height 0.25s ease-in 0.2s;
-  }
-  .compact #bottom-panel {  
-    left: calc(50% - 160px);  
-    width: 320px;
   }
   .compact #zoom-fit-viewer,
   .compact #zoom-fit-page {  
@@ -350,21 +344,6 @@ export const styles = /*html*/`
     font-size: 16px;
     color: var(--${appName}-color-fg-primary-final);
   }
-  #paginator-input {
-    text-align: center; 
-    font-size: 16px;
-    width: 30px;
-    height: 30px;
-    margin: 2px;
-    padding: 0;
-    outline: none;
-    border: none;
-    color: var(--${appName}-color-fg-primary-final);
-    background-color: var(--${appName}-color-primary-final);
-  }
-  #paginator-total {
-    margin: 4px;
-  }
 
   #toggle-previewer {
     margin: 4px;
@@ -415,7 +394,6 @@ export const styles = /*html*/`
     position: absolute;
     display: flex;
     flex-direction: column;
-    justify-content: flex-start;
     overflow: auto;
     left: 160px;
     right: 0;
@@ -483,7 +461,6 @@ export const styles = /*html*/`
     display: flex;
     flex-grow: 0;
     flex-shrink: 0;
-    margin: 10px auto;
     background-color: white;
     box-shadow: 0 0 10px var(--${appName}-color-shadow-final);
   }
@@ -528,53 +505,7 @@ export const styles = /*html*/`
   .page-canvas {
     background-color: white;
   } 
-  
-  .page-text {
-    position: absolute;
-    left: 0;
-    right: 0;
-    top: 0;
-    bottom: 0;
-    margin: 0;
-    padding: 0;
-    overflow: hidden;
-    line-height: 1;
-  }
-  .page-text span {
-    cursor: text;
-    position: absolute;
-    white-space: pre;
-    color: transparent;
-    transform-origin: 0% 0%;
-  }
-  .page-text ::selection {
-    background: var(--${appName}-color-text-selection-final);
-  }
-  .mode-hand .page-text span {
-    cursor: grab;
-  }
-  .dummy-corner {    
-    position: absolute;
-    width: 0;
-    height: 0;
-  }
-  .dummy-corner.bl {
-    bottom: 0;
-    left: 0;
-  }
-  .dummy-corner.br {
-    bottom: 0;
-    right: 0;
-  }
-  .dummy-corner.tr {
-    top: 0;
-    right: 0;
-  }
-  .dummy-corner.tl {
-    top: 0;
-    left: 0;
-  }
-  
+
   .page-annotations {
     width: 0;
     height: 0;
@@ -618,50 +549,6 @@ export const styles = /*html*/`
     height: 100%;
     background: var(--${appName}-color-secondary-tr-final);
   }
-
-  .password-dialog {
-    z-index: 10;
-    pointer-events: all !important;
-  }
-  .password-dialog .form {
-    position: absolute;
-    display: flex;
-    flex-direction: row;
-    justify-content: stretch;
-    align-items: stretch;
-    flex-grow: 0;
-    flex-shrink: 0;
-    left: calc(50% - 160px);
-    top: calc(50% - 25px);
-    width: 320px;
-    height: 50px;
-    background: var(--${appName}-color-primary-tr-final);
-    box-shadow: 0 0 10px var(--${appName}-color-shadow-final);
-  }
-  .password-dialog input {
-    width: 220px;
-    margin: 10px 0 10px 10px;
-    padding: 5px;
-    font-size: 16px;
-    outline: none;
-    border: none;
-    color: var(--${appName}-color-fg-primary-final);
-    background-color: var(--${appName}-color-primary-final);
-  }
-  .password-dialog input::placeholder {
-    font-size: 14px;
-    font-style: italic;
-    color: var(--${appName}-color-fg-primary-final);
-  }
-  .password-dialog .buttons {
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    align-items: center;
-    flex-grow: 1;
-    flex-shrink: 1;
-    width: 100px;
-  } 
   
   .text-dialog {
     z-index: 9;
@@ -1137,5 +1024,129 @@ export const styles = /*html*/`
       top: 30px; 
     }
   }
+  
+  #bottom-panel {
+    left: calc(50% - 200px);
+    width: 400px;
+  }
+  .compact #bottom-panel {  
+    left: calc(50% - 160px);  
+    width: 320px;
+  }
+
+  #viewer {
+    justify-content: flex-start;
+  }
+
+  #paginator-input {
+    text-align: center; 
+    font-size: 16px;
+    width: 30px;
+    height: 30px;
+    margin: 2px;
+    padding: 0;
+    outline: none;
+    border: none;
+    color: var(--${appName}-color-fg-primary-final);
+    background-color: var(--${appName}-color-primary-final);
+  }
+  #paginator-total {
+    margin: 4px;
+  }
+
+  .page-container {
+    margin: 10px auto;
+  }
+  
+  
+  .page-text {
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    line-height: 1;
+  }
+  .page-text span {
+    cursor: text;
+    position: absolute;
+    white-space: pre;
+    color: transparent;
+    transform-origin: 0% 0%;
+  }
+  .page-text ::selection {
+    background: var(--${appName}-color-text-selection-final);
+  }
+  .mode-hand .page-text span {
+    cursor: grab;
+  }
+  .dummy-corner {    
+    position: absolute;
+    width: 0;
+    height: 0;
+  }
+  .dummy-corner.bl {
+    bottom: 0;
+    left: 0;
+  }
+  .dummy-corner.br {
+    bottom: 0;
+    right: 0;
+  }
+  .dummy-corner.tr {
+    top: 0;
+    right: 0;
+  }
+  .dummy-corner.tl {
+    top: 0;
+    left: 0;
+  }
+
+  .password-dialog {
+    z-index: 10;
+    pointer-events: all !important;
+  }
+  .password-dialog .form {
+    position: absolute;
+    display: flex;
+    flex-direction: row;
+    justify-content: stretch;
+    align-items: stretch;
+    flex-grow: 0;
+    flex-shrink: 0;
+    left: calc(50% - 160px);
+    top: calc(50% - 25px);
+    width: 320px;
+    height: 50px;
+    background: var(--${appName}-color-primary-tr-final);
+    box-shadow: 0 0 10px var(--${appName}-color-shadow-final);
+  }
+  .password-dialog input {
+    width: 220px;
+    margin: 10px 0 10px 10px;
+    padding: 5px;
+    font-size: 16px;
+    outline: none;
+    border: none;
+    color: var(--${appName}-color-fg-primary-final);
+    background-color: var(--${appName}-color-primary-final);
+  }
+  .password-dialog input::placeholder {
+    font-size: 14px;
+    font-style: italic;
+    color: var(--${appName}-color-fg-primary-final);
+  }
+  .password-dialog .buttons {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    flex-grow: 1;
+    flex-shrink: 1;
+    width: 100px;
+  }   
 </style>
 `;
